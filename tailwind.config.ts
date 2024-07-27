3085c961c11713a217aae937c0a4fbc4f19ae591
solvman <solvman@gmail.com>
2024-07-26 21:58:54 -0500

    Update title and description in metadata

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -7,9 +7,23 @@ const config: Config = {
     "./app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
+    colors: {
+      purple: "hsl(259, 100%, 65%)",
+      "light-red": "hsl(0, 100%, 67%)",
+      white: "hsl(0, 0%, 100%)",
+      "off-white": "hsl(0, 0%, 94%)",
+      "light-grey": "hsl(0, 0%, 86%)",
+      "smokey-grey": "hsl(0, 1%, 44%)",
+      "off-black": "hsl(0, 0%, 8%)",
+    },
+    fontWeight: {
+      normal: "400",
+      bold: "700",
+      extrabold: "800",
+    },
     extend: {
       fontFamily: {
-        sans: "var(--font-poppins)",
+        serif: "var(--font-poppins)",
       },
     },
   },
